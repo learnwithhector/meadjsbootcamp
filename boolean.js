f7e26ad7c4885f8ad24441eb34cd15e4c4c84303
Hector Mcfoy <learnwithhector@gmail.com>
2021-06-21 11:19:56 +0100

    add boolean.js file to demonstrate comparison operators and boolean data type

---
+++ boolean.js
@@ -0,0 +1,13 @@
+let temp = 100
+let isFreezing = temp >= 32
+
+console.log(isFreezing)
+
+
+let age = 78
+
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(isChild)
+console.log(isSenior)
